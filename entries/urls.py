f2ad7f73ee8b7ffb195ab533c013491b73a1e50d
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-23 21:58:03 +0200

    Fixed home page

--- entries/urls.py
+++ entries/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('entries/delete/<int:item_id>/', views.entry_delete, name='entry_delete'),
     path('entries/', views.entries, name='entries'),
     path('entries/add/', views.entry_add, name='entry_add'),
+    path('', views.entries, name='entries')
 ]
--- lab2/settings.py
+++ lab2/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['protected-bayou-40023-87abb51fa93b.herokuapp.com', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
