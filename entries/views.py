9493bb267b27c08d9655ffaf186a393b82963c2d
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-23 14:52:58 +0200

    Added template for entries

--- entries/views.py
+++ entries/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def entries(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 # Create your views here.
