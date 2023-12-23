0c9fa7f6d8effc3682dc95321ea18848297eb339
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-23 15:01:58 +0200

    Updated index.html. Added list of entries

--- entries/views.py
+++ entries/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Entry
 def entries(request):
+    myentries = Entry.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'myentries': myentries,
+    }
+    return HttpResponse(template.render(context, request))
 # Create your views here.
